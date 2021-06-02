6078f1e8f5158662bd02d33a225f3af9ce389eb8
AfroKakTyC <63326960+AfroKakTyC@users.noreply.github.com>
2021-06-02 14:46:41 +0300

    Unity Earth adn Moon project added

---
+++ EarthAndMoonAR/Assets/Scripts/LightDirectionController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LightDirectionController : MonoBehaviour
+{
+    public Slider lightDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void LightDirection()
+	{
+        float rotationSpeed = 45;
+        Vector3 currentEulerAngles;
+        Quaternion currentRotation = new Quaternion(0, 0, 0, 0);
+        currentEulerAngles = new Vector3(0, lightDirection.value, 0) * 2;
+        currentRotation.eulerAngles = currentEulerAngles;
+        transform.rotation = currentRotation;
+        //gameObject.transform.Rotate(0.0f, lightDirection.value, 0.0f, Space.Self);
+        //transform.RotateAround(Vector3.up, -lightDirection.value);
+        //gameObject.transform.rotation = new Quaternion(gameObject.transform.rotation.x, lightDirection.value, gameObject.transform.rotation.z, 0);
+	}
+
+
+}
---
+++ EarthAndMoonAR/Assets/Scripts/RotateByMouse.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateByMouse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    float rotSpeed = 10;
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+
+        //transform.RotateAround(Vector3.up, -rotX);
+        //transform.Rotate(Vector3.right, rotY);
+        //transform.Rotate(new Vector3(transform.rotation.x, transform.rotation.y + rotY, transform.rotation.z));
+        transform.RotateAround(Vector3.up, -rotX);
+        //transform.RotateAround(Vector3.right, rotY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
